c58865bb6535ab00394854fdf67969581559f820
anakin200a-ai <anakin200a@gmail.com>
2026-04-01 17:59:36 +0300

    seeder for user,emp,store

---
+++ database/seeders/StoreSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Store;
+use Illuminate\Database\Seeder;
+
+class StoreSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Store::create([
+            'storeNumber' => 1,
+        ]);
+
+        Store::create([
+            'storeNumber' => 2,
+        ]);
+
+        Store::create([
+            'storeNumber' => 3,
+        ]);
+    }
+}
